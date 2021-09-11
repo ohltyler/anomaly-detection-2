2b88a7ef3f044909a074816dd8a91b7fd174fc95
Tyler Ohlsen <ohltyler@amazon.com>
2021-09-11 00:10:02 +0000

    Set up methods for query generation

--- src/main/java/org/opensearch/ad/transport/SearchTopAnomalyResultTransportAction.java
+++ src/main/java/org/opensearch/ad/transport/SearchTopAnomalyResultTransportAction.java
@@ -29,32 +29,31 @@ package org.opensearch.ad.transport;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.opensearch.action.ActionListener;
 import org.opensearch.action.search.SearchRequest;
 import org.opensearch.action.search.SearchResponse;
-import org.opensearch.ad.rest.RestSearchTopAnomalyResultAction;
-import org.opensearch.ad.transport.SearchTopAnomalyResultRequest;
-import org.opensearch.ad.transport.SearchTopAnomalyResultResponse;
 import org.opensearch.action.support.ActionFilters;
 import org.opensearch.action.support.HandledTransportAction;
 import org.opensearch.ad.transport.handler.ADSearchHandler;
 import org.opensearch.common.inject.Inject;
-import org.opensearch.common.util.concurrent.ThreadContext;
-import org.opensearch.commons.authuser.User;
+import org.opensearch.index.query.QueryBuilder;
 import org.opensearch.index.query.QueryBuilders;
-import org.opensearch.index.query.TermsQueryBuilder;
+import org.opensearch.search.aggregations.AggregationBuilder;
+import org.opensearch.search.aggregations.AggregationBuilders;
+import org.opensearch.search.aggregations.bucket.composite.CompositeValuesSourceBuilder;
+import org.opensearch.search.aggregations.bucket.composite.TermsValuesSourceBuilder;
 import org.opensearch.search.builder.SearchSourceBuilder;
 import org.opensearch.tasks.Task;
 import org.opensearch.transport.TransportService;
 import org.opensearch.ad.model.AnomalyResultBucket;
 
-import static org.opensearch.ad.constant.CommonErrorMessages.FAIL_TO_SEARCH;
 import static org.opensearch.ad.indices.AnomalyDetectionIndices.ALL_AD_RESULTS_INDEX_PATTERN;
-import static org.opensearch.ad.util.ParseUtils.getUserContext;
-import static org.opensearch.ad.util.RestHandlerUtils.getSourceContext;
-import static org.opensearch.ad.util.RestHandlerUtils.wrapRestActionListener;
 
 public class SearchTopAnomalyResultTransportAction extends HandledTransportAction<SearchTopAnomalyResultRequest, SearchTopAnomalyResultResponse> {
     private ADSearchHandler searchHandler;
@@ -104,18 +103,42 @@ public class SearchTopAnomalyResultTransportAction extends HandledTransportActio
     private SearchRequest generateSearchRequest(SearchTopAnomalyResultRequest request) {
         SearchRequest searchRequest = new SearchRequest().indices(index);
 
-        // TODO: given the request, generate a SearchRequest to pass to the SearchHandler
+        // Generate the query for the request
+        QueryBuilder query = generateQuery(request);
 
-        // Match name with existing detectors
-        TermsQueryBuilder query = QueryBuilders.termsQuery("name.keyword", "test-detector-name");
-        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder().query(query);
+        // Generate the aggregation for the request
+        AggregationBuilder aggregation = generateAggregation(request);
+
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder().query(query).aggregation(aggregation);
         searchRequest.source(searchSourceBuilder);
 
-
-        logger.info("\n\nsearch request query:\n" + searchRequest.source().query());
-
+        // TODO: remove these 5 lines later. Just for debugging purposes / pretty printing in console
+        JsonParser parser = new JsonParser();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonElement el = parser.parse(searchRequest.source().toString());
+        String prettyJson = gson.toJson(el);
+        logger.info("\n\nsearch request:\n" + prettyJson);
 
         return searchRequest;
+    }
+
+    private QueryBuilder generateQuery(SearchTopAnomalyResultRequest request) {
+        QueryBuilder query = QueryBuilders.boolQuery().filter(QueryBuilders.termQuery("detector_id", request.getDetectorId()));
+
+        // TODO: add filter for historical v. realtime
+
+        return query;
+    }
+
+    // Generates a composite aggregation, with the sources being the different
+    // category fields the user wants to filter by
+    private AggregationBuilder generateAggregation(SearchTopAnomalyResultRequest request) {
+        List<CompositeValuesSourceBuilder<?>> sources = new ArrayList<>();
+        sources.add(new TermsValuesSourceBuilder("field1").field("field1"));
+        AggregationBuilder aggregation = AggregationBuilders.composite("multi_buckets", sources);
+
+
+        return aggregation;
     }
 
 
