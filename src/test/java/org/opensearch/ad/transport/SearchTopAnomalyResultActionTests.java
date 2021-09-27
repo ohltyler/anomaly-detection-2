ebbae6f1ac1629964e30fd0e31da00baf353b702
Tyler Ohlsen <ohltyler@amazon.com>
2021-09-27 17:34:25 +0000

    Compatible with 1.0

--- src/test/java/org/opensearch/ad/transport/SearchTopAnomalyResultActionTests.java
+++ src/test/java/org/opensearch/ad/transport/SearchTopAnomalyResultActionTests.java
@@ -9,37 +9,16 @@
  * GitHub history for details.
  */
 
-/*
- * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
-
 package org.opensearch.ad.transport;
 
 import org.junit.Test;
-import org.opensearch.action.search.SearchResponse;
 import org.opensearch.ad.ADIntegTestCase;
-import org.opensearch.ad.HistoricalAnalysisIntegTestCase;
-import org.opensearch.ad.TestHelpers;
 import org.opensearch.ad.constant.CommonName;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.time.Instant;
 import java.util.List;
-
-import static org.opensearch.ad.TestHelpers.matchAllRequest;
 
 public class SearchTopAnomalyResultActionTests extends ADIntegTestCase {
 
